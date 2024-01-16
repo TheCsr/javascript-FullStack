5714b8b0c80c63a87e10f1103f8a7ce75e96386f
TheCsr <csrrocz@gmail.com>
2024-01-16 23:51:29 +0100

    filter and favourite list implementation

--- BeerApp - Senior/src/views/Home/index.tsx
+++ BeerApp - Senior/src/views/Home/index.tsx
@@ -1,16 +1,83 @@
-import { useEffect, useState } from 'react';
-import { fetchData } from './utils';
-import { Beer } from '../../types';
-import { Link as RouterLink } from 'react-router-dom';
-import { Button, Checkbox, Paper, TextField, Link } from '@mui/material';
-import styles from './Home.module.css';
+import { useEffect, useState } from "react";
+import { fetchData } from "./utils";
+import { Beer } from "../../types";
+import { Link as RouterLink } from "react-router-dom";
+import {
+  Button,
+  Checkbox,
+  Paper,
+  TextField,
+  Link,
+  Card,
+  CardContent,
+} from "@mui/material";
+import styles from "./Home.module.css";
+import BeerList from "../BeerList";
 
 const Home = () => {
   const [beerList, setBeerList] = useState<Array<Beer>>([]);
+  const [filterTerm, setFilterTerm] = useState<string>("");
   const [savedList, setSavedList] = useState<Array<Beer>>([]);
-
+  const [selected, setSelected] = useState<string[]>([]);
   // eslint-disable-next-line
   useEffect(fetchData.bind(this, setBeerList), []);
+
+  const handlefetchBeer = () => {
+    fetchData(setBeerList);
+  };
+
+  const handleCheckboxChange = (beerId: string) => {
+    const isSelected = selected.includes(beerId);
+    if (isSelected) {
+      const selectedIds = selected.filter((id) => id !== beerId);
+      localStorage.setItem("selected", JSON.stringify(selectedIds));
+      setSelected(selectedIds);
+      const removeFromSavedList = savedList.filter(
+        (beer) => beer.id !== beerId
+      );
+      localStorage.setItem("savedList", JSON.stringify(removeFromSavedList));
+      setSavedList(removeFromSavedList);
+    } else {
+      const selectedIds = [...selected, beerId];
+      localStorage.setItem("selected", JSON.stringify(selectedIds));
+      setSelected(selectedIds);
+      const newObject: Beer | undefined = beerList.find(
+        (obj) => obj.id === beerId
+      );
+      if (newObject) {
+        // Add the new Beer object to savedList
+        const addToSavedList: Beer[] = [...savedList, newObject];
+        localStorage.setItem("savedList", JSON.stringify(addToSavedList));
+        setSavedList(addToSavedList);
+      }
+    }
+  };
+
+  // Filter the beer list based on the input filter term
+  const filteredBeerList = beerList.filter((beer) =>
+    beer.name.toLowerCase().includes(filterTerm.toLowerCase())
+  );
+
+  const handleRemoveList = () => {
+    setSavedList([]);
+    setSelected([]);
+    localStorage.removeItem('savedList');
+    localStorage.removeItem('selected');
+  };
+
+  useEffect(() => {
+    // Load favorite list from local storage
+    const storedDataArray = localStorage.getItem("savedList");
+    const storedSelectedIds = localStorage.getItem("selected");
+    if (storedDataArray) {
+      let storedSavedList: Beer[] = JSON.parse(storedDataArray) || [];
+      setSavedList(storedSavedList);
+    }
+    if(storedSelectedIds) {
+      let storedSelectedList = JSON.parse(storedSelectedIds) || [];
+      setSelected(storedSelectedList);
+    }
+  }, []);
 
   return (
     <article>
@@ -19,13 +86,26 @@ const Home = () => {
           <Paper>
             <div className={styles.listContainer}>
               <div className={styles.listHeader}>
-                <TextField label='Filter...' variant='outlined' />
-                <Button variant='contained'>Reload list</Button>
+                <TextField
+                  label="Filter..."
+                  variant="outlined"
+                  value={filterTerm}
+                  onChange={(e) => setFilterTerm(e.target.value)}
+                />
+                <div>
+                  <Button variant="contained" onClick={handlefetchBeer}>
+                    Reload list
+                  </Button>
+                </div>
               </div>
+              <div></div>
               <ul className={styles.list}>
-                {beerList.map((beer, index) => (
+                {filteredBeerList.map((beer, index) => (
                   <li key={index.toString()}>
-                    <Checkbox />
+                    <Checkbox
+                      checked={selected.includes(beer.id)}
+                      onChange={() => handleCheckboxChange(beer.id)}
+                    />
                     <Link component={RouterLink} to={`/beer/${beer.id}`}>
                       {beer.name}
                     </Link>
@@ -39,7 +119,11 @@ const Home = () => {
             <div className={styles.listContainer}>
               <div className={styles.listHeader}>
                 <h3>Saved items</h3>
-                <Button variant='contained' size='small'>
+                <Button
+                  variant="contained"
+                  size="small"
+                  onClick={handleRemoveList}
+                >
                   Remove all items
                 </Button>
               </div>
